781f3be0440d0872da1487cab747956c9b83dd24
vinickpkx <vinickpkx@gmail.com>
2020-09-15 16:37:02 -0300

    Utilizando Loop de For para replicar a validação e calculo de IMC para cada paciente existente

--- js/principal.js
+++ js/principal.js
@@ -1,32 +1,37 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-let paciente = document.querySelector("#primeiroPaciente");
+let pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes)
 
-let tdPeso = paciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
+for (let i = 0; i < pacientes.length; i++) {
+    const paciente = pacientes[i];
 
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
+    let tdPeso = paciente.querySelector(".info-peso");
+    let peso = tdPeso.textContent;
 
-let tdImc = paciente.querySelector(".info-imc")
+    let tdAltura = paciente.querySelector(".info-altura");
+    let altura = tdAltura.textContent;
 
-let pesoEhValido = true;
-let alturaEhValida = true;
+    let tdImc = paciente.querySelector(".info-imc")
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso Inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!"
-}
+    let pesoEhValido = true;
+    let alturaEhValida = true;
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura Inválido");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválido!"
-}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso Inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!"
+    }
 
-if (alturaEhValida && pesoEhValido) {
-    let imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura Inválido");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválido!"
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        let imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }

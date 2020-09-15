badace9f10a7558b2a454aee52f9167d045f602a
vinickpkx <vinickpkx@gmail.com>
2020-09-15 15:37:45 -0300

    Aprendendo a Coletar dados usando QuerySelector

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,10 @@
 let titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiroPaciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;

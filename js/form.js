456267e4cf4eb5df8d2f45be75a96d157c98f66b
vinickpkx <vinickpkx@gmail.com>
2020-09-17 14:25:15 -0300

    Criação da validação para formulario e correção de erros do programa

--- js/form.js
+++ js/form.js
@@ -1,6 +1,4 @@
-let botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-let obtemInfosForm = (form) => {
+let obtemPacienteDoFormulario = (form) => {
 
     let paciente = {
         nome: form.nome.value,
@@ -11,52 +9,92 @@ let obtemInfosForm = (form) => {
     }
 
     return paciente;
-};
+}
+
+let montaTr = (paciente) => {
+    let pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
 
 let montaTd = (dado, classe) => {
     let td = document.createElement("td");
-    td.textContent = dado;
     td.classList.add(classe);
+    td.textContent = dado;
 
     return td;
 }
 
-let montaTr = (paciente) => {
-    let pacienteTR = document.createElement("tr");
-    pacienteTR.classList.add("paciente");
+let validaPaciente = (paciente) => {
 
-    let nomeTd = montaTd(paciente.nome, "info-nome");
-    let pesoTd = montaTd(paciente.peso, "info-peso");
-    let alturaTd = montaTd(paciente.altura, "info-altura");
-    let gorduraTd = montaTd(paciente.gordura, "info-gordura");
-    let imcTd = montaTd(paciente.imc, "info-imc");
+    let erros = [];
 
-    pacienteTR.appendChild(nomeTd);
-    pacienteTR.appendChild(alturaTd);
-    pacienteTR.appendChild(pesoTd);
-    pacienteTR.appendChild(gorduraTd);
-    pacienteTR.appendChild(imcTd);
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
 
-    return pacienteTR;
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
 }
 
+let exibeMensagensDeErro = (erros) => {
+    let ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach((erro) => {
+        let li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+let botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", (event) => {
     event.preventDefault();
 
-    let form = document.querySelector("#formAdiciona");
+    let form = document.querySelector("#form-adiciona");
 
-    //Extraindo infos do paciente do form
-    let paciente = obtemInfosForm(form);
+    let paciente = obtemPacienteDoFormulario(form);
 
-    console.log(paciente);
+    let pacienteTr = montaTr(paciente);
 
-    //cria tr e td
-    let pacienteTR = montaTr(paciente);
+    let erros = validaPaciente(paciente);
 
-    //Adicionando paciente em tabela
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return;
+    }
 
     let tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTR);
-
+    tabela.appendChild(pacienteTr);
     form.reset();
+
+    let mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });

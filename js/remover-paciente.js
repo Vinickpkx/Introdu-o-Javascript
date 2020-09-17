a0dbc5aeeeea80f6f19c64bb4c9a1d4adc683e3d
vinickpkx <vinickpkx@gmail.com>
2020-09-17 15:05:58 -0300

    Evento que deleta paciente com animação de fadeOut adicionado

---
+++ js/remover-paciente.js
@@ -0,0 +1,9 @@
+let tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", (event) => {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(() => {
+        event.target.parentNode.remove();
+    }, 500);
+});
